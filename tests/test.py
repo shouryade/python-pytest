efab36f2d2af75085ec043f212561fb44bd65087
AM0312 <ansh.midha0312@gmail.com>
2024-05-07 00:58:31 +0530

    files

---
+++ tests/test.py
@@ -0,0 +1,18 @@
+# test_app.py
+import pytest
+from app import app
+
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
+
+def test_index(client):
+    response = client.get('/')
+    assert response.status_code == 200
+    assert b"Hello, World!" in response.data
+
+def test_invalid_route(client):
+    response = client.get('/invalid-route')
+    assert response.status_code == 404
